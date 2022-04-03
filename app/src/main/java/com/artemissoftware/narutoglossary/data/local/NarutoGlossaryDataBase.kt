5e919f2c4c32349568ebe7934867e845794528d4
gustavo <gustavo.isaac@gmail.com>
2022-04-03 13:25:47 +0100

    Create DatabaseConverter class

--- app/src/main/java/com/artemissoftware/narutoglossary/data/local/NarutoGlossaryDataBase.kt
+++ app/src/main/java/com/artemissoftware/narutoglossary/data/local/NarutoGlossaryDataBase.kt
@@ -2,6 +2,7 @@ package com.artemissoftware.narutoglossary.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.artemissoftware.narutoglossary.data.local.dao.HeroDao
 import com.artemissoftware.narutoglossary.data.local.dao.HeroRemoteKeyDao
 import com.artemissoftware.narutoglossary.domain.model.Hero
@@ -12,6 +13,7 @@ import com.artemissoftware.narutoglossary.domain.model.HeroRemoteKey
         HeroRemoteKey::class
     ],
     version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class NarutoGlossaryDataBase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
