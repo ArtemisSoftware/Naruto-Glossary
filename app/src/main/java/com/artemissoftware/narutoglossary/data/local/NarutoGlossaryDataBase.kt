d589c2053e0bd135d8bebb1225091141e5ffd880
gustavo <gustavo.isaac@gmail.com>
2022-04-03 13:22:44 +0100

    Create HeroRemoteKey Entity & Dao

--- app/src/main/java/com/artemissoftware/narutoglossary/data/local/NarutoGlossaryDataBase.kt
+++ app/src/main/java/com/artemissoftware/narutoglossary/data/local/NarutoGlossaryDataBase.kt
@@ -3,14 +3,18 @@ package com.artemissoftware.narutoglossary.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.artemissoftware.narutoglossary.data.local.dao.HeroDao
+import com.artemissoftware.narutoglossary.data.local.dao.HeroRemoteKeyDao
 import com.artemissoftware.narutoglossary.domain.model.Hero
+import com.artemissoftware.narutoglossary.domain.model.HeroRemoteKey
 
 @Database(entities = [
         Hero::class,
+        HeroRemoteKey::class
     ],
     version = 1)
 abstract class NarutoGlossaryDataBase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 
 }
