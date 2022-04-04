fad4cc558f258265bb649ef4846734de262af92d
gustavo <gustavo.isaac@gmail.com>
2022-04-04 18:29:07 +0100

    Design RatingWidget - Empty Star

---
+++ app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/components/EmptyStar.kt
@@ -0,0 +1,61 @@
+package com.artemissoftware.narutoglossary.presentation.screens.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.vector.PathParser
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.narutoglossary.R
+import com.artemissoftware.narutoglossary.ui.theme.StarColor
+
+@Composable
+fun EmptyStar(
+    starPath: Path,
+    starPathBounds: Rect,
+    scaleFactor: Float
+) {
+    Canvas(modifier = Modifier.size(24.dp)) {
+        val canvasSize = size
+        scale(scale = scaleFactor) {
+            val pathWidth = starPathBounds.width
+            val pathHeight = starPathBounds.height
+            val left = (canvasSize.width / 2f) - (pathWidth / 1.7f)
+            val top = (canvasSize.height / 2f) - (pathHeight / 1.7f)
+
+            translate(left = left, top = top) {
+                drawPath(
+                    path = starPath,
+                    color = LightGray.copy(alpha = 0.5f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun EmptyStarPreview() {
+    val starPathString = stringResource(id = R.string.star_path)
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString).toPath()
+    }
+    val starPathBounds = remember {
+        starPath.getBounds()
+    }
+    EmptyStar(
+        starPath = starPath,
+        starPathBounds = starPathBounds,
+        scaleFactor = 3f
+    )
+}
