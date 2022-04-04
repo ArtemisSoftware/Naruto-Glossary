f8ff0341b8ce86685d134b7c9db4e799e8baff48
gustavo <gustavo.isaac@gmail.com>
2022-04-04 16:37:34 +0100

    Home Screen - Design Top Bar

--- app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,21 @@
 package com.artemissoftware.narutoglossary.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.artemissoftware.narutoglossary.presentation.screens.home.composables.HomeTopBar
 
 @Composable
 fun HomeScreen() {
-    
+
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        },
+        content = {
+//            ListContent(
+//                heroes = allHeroes,
+//                navController = navController
+//            )
+        }
+    )
 }
---
+++ app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/home/composables/HomeTopBar.kt
@@ -0,0 +1,39 @@
+package com.artemissoftware.narutoglossary.presentation.screens.home.composables
+
+import android.content.res.Configuration
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.artemissoftware.narutoglossary.R
+import com.artemissoftware.narutoglossary.ui.theme.topAppBarBackgroundColor
+import com.artemissoftware.narutoglossary.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Explore",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
