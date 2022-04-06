d4658161fa87b68ecfab69670a161e4b8d219f1c
gustavo <gustavo.isaac@gmail.com>
2022-04-06 19:03:57 +0100

    Display Shimmer Effect

--- app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/common/ListContent.kt
+++ app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/common/ListContent.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import com.artemissoftware.narutoglossary.domain.model.Hero
+import com.artemissoftware.narutoglossary.presentation.screens.components.ShimmerEffect
 import com.artemissoftware.narutoglossary.ui.theme.SMALL_PADDING
 
 @Composable
@@ -19,19 +21,49 @@ fun ListContent(
 
     Log.d("LazyColumn", heroes.loadState.toString())
 
-    LazyColumn(
-        contentPadding = PaddingValues(all = SMALL_PADDING),
-        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING)
-    ) {
-        items(
-            items = heroes,
-            key = { hero ->
-                hero.id
+    val result = HandlePagingResult(heroes = heroes)
+
+    if (result) {
+        LazyColumn(
+            contentPadding = PaddingValues(all = SMALL_PADDING),
+            verticalArrangement = Arrangement.spacedBy(SMALL_PADDING)
+        ) {
+            items(
+                items = heroes,
+                key = { hero ->
+                    hero.id
+                }
+            ) { hero ->
+                hero?.let {
+                    HeroItem(hero = it, navController = navController)
+                }
             }
-        ) { hero ->
-            hero?.let {
-                HeroItem(hero = it, navController = navController)
+        }
+    }
+}
+
+@Composable
+fun HandlePagingResult(
+    heroes: LazyPagingItems<Hero>
+): Boolean {
+    heroes.apply {
+        val error = when {
+            loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+            loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+            loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+            else -> null
+        }
+
+        return when {
+            loadState.refresh is LoadState.Loading -> {
+                ShimmerEffect()
+                false
             }
+            error != null -> {
+                //--EmptyScreen(error = error)
+                false
+            }
+            else -> true
         }
     }
 }
