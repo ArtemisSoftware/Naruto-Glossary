5a32e570d4ee7d7c3531af011baf4cd7defea584
gustavo <gustavo.isaac@gmail.com>
2022-04-17 17:11:22 +0100

    Details Screen - Design BackgroundContent

---
+++ app/src/main/java/com/artemissoftware/narutoglossary/presentation/screens/details/composables/BottomSheetContent.kt
@@ -0,0 +1,126 @@
+package com.artemissoftware.narutoglossary.presentation.screens.details.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.artemissoftware.narutoglossary.R
+import com.artemissoftware.narutoglossary.domain.model.Hero
+import com.artemissoftware.narutoglossary.presentation.screens.components.OrderedList
+import com.artemissoftware.narutoglossary.ui.theme.INFO_ICON_SIZE
+import com.artemissoftware.narutoglossary.ui.theme.LARGE_PADDING
+import com.artemissoftware.narutoglossary.ui.theme.MEDIUM_PADDING
+import com.artemissoftware.narutoglossary.ui.theme.titleColor
+import com.artemissoftware.narutoglossary.util.Constants
+
+@Composable
+fun BottomSheetContent(
+    selectedHero: Hero,
+    infoBoxIconColor: Color = MaterialTheme.colors.primary,
+    sheetBackgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = MaterialTheme.colors.titleColor
+) {
+    Column(
+        modifier = Modifier
+            .background(sheetBackgroundColor)
+            .padding(all = LARGE_PADDING)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = LARGE_PADDING),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(INFO_ICON_SIZE)
+                    .weight(2f),
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(id = R.string.app_logo),
+                tint = contentColor
+            )
+            Text(
+                modifier = Modifier
+                    .weight(8f),
+                text = selectedHero.name,
+                color = contentColor,
+                fontSize = MaterialTheme.typography.h4.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = MEDIUM_PADDING),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            InfoBox(
+                icon = painterResource(id = R.drawable.ic_bolt),
+                iconColor = infoBoxIconColor,
+                bigText = "${selectedHero.power}",
+                smallText = stringResource(R.string.power),
+                textColor = contentColor
+            )
+            InfoBox(
+                icon = painterResource(id = R.drawable.ic_calendar),
+                iconColor = infoBoxIconColor,
+                bigText = selectedHero.month,
+                smallText = stringResource(R.string.month),
+                textColor = contentColor
+            )
+            InfoBox(
+                icon = painterResource(id = R.drawable.ic_cake),
+                iconColor = infoBoxIconColor,
+                bigText = selectedHero.day,
+                smallText = stringResource(R.string.birthday),
+                textColor = contentColor
+            )
+        }
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.about),
+            color = contentColor,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier
+                .alpha(ContentAlpha.medium)
+                .padding(bottom = MEDIUM_PADDING),
+            text = selectedHero.about,
+            color = contentColor,
+            fontSize = MaterialTheme.typography.body1.fontSize,
+            maxLines = Constants.ABOUT_TEXT_MAX_LINES
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            OrderedList(
+                title = stringResource(R.string.family),
+                items = selectedHero.family,
+                textColor = contentColor
+            )
+            OrderedList(
+                title = stringResource(R.string.abilities),
+                items = selectedHero.abilities,
+                textColor = contentColor
+            )
+            OrderedList(
+                title = stringResource(R.string.nature_types),
+                items = selectedHero.natureTypes,
+                textColor = contentColor
+            )
+        }
+    }
+}
